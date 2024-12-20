275f16371dc3f44dafbd7e2331035ff7089e62b6
Ridham <ridhamsharma172@gmail.com>
2024-12-20 21:32:04 +0530

    More tutorials

---
+++ string_functions.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    // strcat Function
+    // char helloWorld[6] = "Hello";
+    // char world[] = " World";
+
+    // strcat(helloWorld, world);
+
+    // printf(helloWorld);
+
+    // strlen function
+    // char str[] = "Hello, I am under the water";
+    // printf("%d", strlen(str));
+    // strlen(identifier);
+
+    // strcpy function
+    // char a[20] = "Hello, World\n";
+    // char b[] = "asdasd";
+    // printf(a);
+    // strcpy(a, b);
+    // printf(a);
+    
+    // strcmp function
+    // char a[] = "String";
+    // char b[] = "String";
+
+    // if (!strcmp(a, b)) {
+    //     printf("They are equal");
+    // }
+
+    // strrev function
+    // char a[] = "WhW";
+    // strrev(a);
+    // printf("a is WhW and its reversed string is %s", a);
+
+    return 0;
+}
